6d7f5dff2900183aa7b48fbd6bf226ee5c7a1081
al3xdiaz <alexleonel96@hotmail.com>
2023-10-03 13:51:13 -0600

    [fix] docker container command

--- db/conection.go
+++ db/conection.go
@@ -1,18 +1,22 @@
 package db
 
 import (
+	"log"
+	"os"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	"log"
 )
-var DSN = "golang:golang@tcp(db:3306)/gorm?charset=utf8mb4&parseTime=True&loc=Local"
+
+var DSN = os.Getenv("DSN")
 var DB *gorm.DB
+
 func Connect() {
 	var err error
 	DB, err = gorm.Open(mysql.Open(DSN), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err)
-	}else {
+	} else {
 		log.Println("Database connection established")
 	}
 }
