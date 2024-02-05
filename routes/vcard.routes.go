a95d24e1da88a16405f1f0b929f37c48483817bf
al3xdiaz <alexleonel96@hotmail.com>
2024-02-04 22:45:38 -0600

    [fix] vcard service and route files

--- routes/vcard.routes.go
+++ routes/vcard.routes.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"net/http"
 
+	"github.com/al3xdiaz/go-server/models"
 	"github.com/al3xdiaz/go-server/services"
 	request "github.com/al3xdiaz/go-server/utils"
 	"github.com/gorilla/mux"
@@ -11,13 +12,46 @@ import (
 func VCard(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	username := params["username"]
-
 	service := services.VCardService{}
-	err := service.CreateVCard(username, &w)
+	err := service.CreateVCard(username, func(user models.User) {
+		w.Header().Set("Content-Disposition", "attachment; filename="+username+".vcf")
+		w.Header().Set("Content-Type", r.Header.Get("Content-Type"))
+		w.Write([]byte("BEGIN:VCARD\r\n"))
+		w.Write([]byte("VERSION:2.1\r\n"))
+		w.Write([]byte("N:" + user.Profile.FirstName + "\r\n"))
+		w.Write([]byte("FN:" + user.Profile.FirstName + " " + user.Profile.LastName + "\r\n"))
+		w.Write([]byte("NICKNAME:" + user.UserName + "\r\n"))
+		w.Write([]byte("TEL;CELL:" + user.Profile.Telephone + "\r\n"))
+		w.Write([]byte("EMAIL:" + user.Email + "\r\n"))
+		if user.Profile.Photo != "" {
+			w.Write([]byte("PHOTO;VALUE=URI:" + user.Profile.Photo + "\r\n"))
+		}
+		if user.Profile.Facebook != "" {
+			w.Write([]byte("X-SOCIALPROFILE;TYPE=facebook:" + user.Profile.Facebook + "\r\n"))
+		}
+		if user.Profile.Twitter != "" {
+			w.Write([]byte("X-SOCIALPROFILE;TYPE=twitter:" + user.Profile.Twitter + "\r\n"))
+		}
+		if user.Profile.Linkedin != "" {
+			w.Write([]byte("X-SOCIALPROFILE;TYPE=linkedin:" + user.Profile.Linkedin + "\r\n"))
+		}
+		if user.Profile.Github != "" {
+			w.Write([]byte("X-SOCIALPROFILE;TYPE=github:" + user.Profile.Github + "\r\n"))
+		}
+		if user.Profile.Instagram != "" {
+			w.Write([]byte("X-SOCIALPROFILE;TYPE=instagram:" + user.Profile.Instagram + "\r\n"))
+		}
+		if user.Profile.Youtube != "" {
+			w.Write([]byte("X-SOCIALPROFILE;TYPE=youtube:" + user.Profile.Youtube + "\r\n"))
+		}
+		if user.Profile.Website != "" {
+			w.Write([]byte("URL:" + user.Profile.Website + "\r\n"))
+		}
+
+		w.Write([]byte("END:VCARD\r\n"))
+	})
 	if err != nil {
 		request.NotFound(w, err.Error())
 		return
 	}
-	w.Header().Set("Content-Disposition", "attachment; filename="+username+".vcf")
-	w.Header().Set("Content-Type", r.Header.Get("Content-Type"))
 }
--- services/vcard.service.go
+++ services/vcard.service.go
@@ -2,15 +2,15 @@ package services
 
 import (
 	"errors"
-	"net/http"
 
 	"github.com/al3xdiaz/go-server/db"
+	"github.com/al3xdiaz/go-server/models"
 )
 
 type VCardService struct {
 }
 
-func (o VCardService) CreateVCard(username string, w *http.ResponseWriter) error {
+func (o VCardService) CreateVCard(username string, next func(user models.User)) error {
 	service := ProfileService{
 		DB: db.DB,
 	}
@@ -21,39 +21,6 @@ func (o VCardService) CreateVCard(username string, w *http.ResponseWriter) error
 	if user.Profile.Telephone == "" {
 		return errors.New("the user not has telephone")
 	}
-
-	(*w).Write([]byte("BEGIN:VCARD\r\n"))
-	(*w).Write([]byte("VERSION:2.1\r\n"))
-	(*w).Write([]byte("N:" + user.Profile.FirstName + "\r\n"))
-	(*w).Write([]byte("FN:" + user.Profile.FirstName + " " + user.Profile.LastName + "\r\n"))
-	(*w).Write([]byte("NICKNAME:" + user.UserName + "\r\n"))
-	(*w).Write([]byte("TEL;CELL:" + user.Profile.Telephone + "\r\n"))
-	(*w).Write([]byte("EMAIL:" + user.Email + "\r\n"))
-	if user.Profile.Photo != "" {
-		(*w).Write([]byte("PHOTO;VALUE=URI:" + user.Profile.Photo + "\r\n"))
-	}
-	if user.Profile.Facebook != "" {
-		(*w).Write([]byte("X-SOCIALPROFILE;TYPE=facebook:" + user.Profile.Facebook + "\r\n"))
-	}
-	if user.Profile.Twitter != "" {
-		(*w).Write([]byte("X-SOCIALPROFILE;TYPE=twitter:" + user.Profile.Twitter + "\r\n"))
-	}
-	if user.Profile.Linkedin != "" {
-		(*w).Write([]byte("X-SOCIALPROFILE;TYPE=linkedin:" + user.Profile.Linkedin + "\r\n"))
-	}
-	if user.Profile.Github != "" {
-		(*w).Write([]byte("X-SOCIALPROFILE;TYPE=github:" + user.Profile.Github + "\r\n"))
-	}
-	if user.Profile.Instagram != "" {
-		(*w).Write([]byte("X-SOCIALPROFILE;TYPE=instagram:" + user.Profile.Instagram + "\r\n"))
-	}
-	if user.Profile.Youtube != "" {
-		(*w).Write([]byte("X-SOCIALPROFILE;TYPE=youtube:" + user.Profile.Youtube + "\r\n"))
-	}
-	if user.Profile.Website != "" {
-		(*w).Write([]byte("URL:" + user.Profile.Website + "\r\n"))
-	}
-
-	(*w).Write([]byte("END:VCARD\r\n"))
+	next(user)
 	return nil
 }
