50e2899a6330f53a0d5cc842cfaf07a9e4cd64a8
al3xdiaz <alexleonel96@hotmail.com>
2024-03-07 13:35:45 -0600

    [fix]  reflect errors

--- server.go
+++ server.go
@@ -73,11 +73,10 @@ func RunServer() {
 		http.MethodDelete,
 		http.MethodOptions,
 	})
-	// origins := handlers.AllowedOrigins([]string{"https://alex.chaoticteam.com", "http://localhost:3000"})
-	var sites []string
-	db.DB.Model(models.Site{}).Pluck("url", sites)
-	log.Print(sites)
-	origins := handlers.AllowedOrigins(sites)
+	var originsString []string = []string{"*"}
+	db.DB.Model(&models.Site{}).Pluck("url", &originsString)
+	log.Print(originsString)
+	origins := handlers.AllowedOrigins(originsString)
 	headers := handlers.AllowedHeaders([]string{"Authorization", "content-type", "X-Requested-With"})
 	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(credentials, methods, origins, headers)(router)))
 }
